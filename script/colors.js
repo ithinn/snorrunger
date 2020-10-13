8d68f7eeca1138e1dc95836c5eba77a3dd5f5f88
ithinn <ithinn@gmail.com>
2020-10-13 09:56:48 +0200

    nav changes

---
+++ script/colors.js
@@ -0,0 +1,23 @@
+const colorCodes = 
+    [
+    {
+        clrName: 'red',
+        clrHex: "#c1272d"
+    },
+    {
+        clrName: 'green',
+        clrHex: "#707312"
+    },
+    {
+        clrName: 'blue',
+        clrHex: "#96ACD9"
+    },
+    {
+        clrName: 'black',
+        clrHex: "#333333"
+    },
+    {
+        clrName: 'gray',
+        clrHex: "#808080"
+    }
+];
--- script/nav.js
+++ script/nav.js
@@ -1,8 +1,15 @@
 
+//Global
 const jakker = document.querySelector("#jakker");
 let c_u_arr = ['Dunjakker', 'Skalljakker', 'Ulljakker'];
 let allSizes = [86, 92, 98, 104, 110, 116, 122];
+let qualities = ['vannavstøtende', 'vindtett', 'vanntett', 'varm', 'strikk'];
+let colors = ['green', 'red', 'blue', 'black', 'gray'];
+let emptArray = [];
+let qualFilterArr = [];
 
+
+//Functions
 const filterJackets = () => {
 
 //Filtrerer ut jakkeelementer
@@ -14,6 +21,8 @@ let jacketArray = products.filter(function(product) {
 addObjects(jacketArray)
 addUnderCat(jacketArray);
 addSizes(jacketArray);
+addAttribute(jacketArray);
+addColor(jacketArray);
 }
 
 jakker.addEventListener("click", filterJackets);
@@ -36,7 +45,7 @@ const addUnderCat = (array) => {
     }
 }
 
-let emptArray = [];
+
 const addSizes = (array) => {
     let html ="";
     
@@ -56,5 +65,66 @@ const addSizes = (array) => {
             }
         document.getElementById("size_content").innerHTML = html;
     }
-console.log(emptArray);
 }
+
+
+//Adds "Funksjon" filters
+const addAttribute = (a) => {
+    let html = "";
+
+    for (let i=0; i<a.length; i++) {
+        
+        for (let j = 0; j < qualities.length; j++) {
+
+            for (let k = 0; k < a[i].quality1.length; k++) {
+                if(a[i].quality1[k] === qualities[j] )          {
+                    html += `
+                    <div class="tag">
+                    <p>${a[i].quality1[k]}</p>
+                    </div>
+                    `
+                    qualFilterArr.push(a[i].quality1[k]);
+                }
+            }
+        }
+        document.getElementById("att_content").innerHTML = html;
+    } 
+}
+
+
+//Adds color filters
+
+const addColor = (a) => {
+    let html = "";
+
+    for (let i = 0; i < a.length; i++) {
+
+        for (let j = 0; j < colors.length; j++) {
+
+            for (let k = 0; k < a[i].color.length; k++) {
+
+                if(a[i].color[k] === colors[j]) {
+
+                    html += `
+                    <div id="clr" class="clr_large"></div>
+                    `
+            
+                }
+
+                if(a[i].color[k] ==="red") {
+                    document.getElementById("clr").style.backgroundColor = "#c1272d";
+                }
+                
+            }
+        }
+
+    document.getElementById("color_content").innerHTML = html;
+    }; 
+}
+
+
+// switch(colors) {
+//     case "red":
+
+        
+// }
